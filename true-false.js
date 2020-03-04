d57e938e30bfe3fcc0a9e26677ea9d44f5175f67
ApelMahmud65 <apelmahmud.kpi@gmail.com>
2020-03-04 22:16:55 +0800

    repository is done

---
+++ true-false.js
@@ -0,0 +1,18 @@
+//"" = false
+//null = false
+//NaN = false
+// undefined = false
+// false = false
+// 0 = false
+//truthy
+//'0', " ", [];
+
+
+let name = 12;
+
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
