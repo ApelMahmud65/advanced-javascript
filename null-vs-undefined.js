68268e7eea358a8cb33ad9b97d545b09c4bb0462
ApelMahmud65 <apelmahmud.kpi@gmail.com>
2020-04-29 10:47:27 +0800

    null vs undefined cleared for interview

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,9 +1,27 @@
-//let pakhi;
+//How to get undefined
 
-function add(num1,num2){
-    console.log(num1+num2);
-    return;
+// let pakhi;
+// console.log(pakhi);
 
-}
-const result = add(22,22);
-console.log(result);
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return;
+// }
+// var result = add(10, 5);
+// console.log(result)
+
+// function add(num1, num2){
+//     console.log(num1, num2)
+// }
+// add(10)
+
+// const premik = {name:'smart boy', phone: 2345};
+// console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+
+
+
+//How to get null
