39e95dc6989e72b1520e6c406dfb1b6e41a4153e
ApelMahmud65 <apelmahmud.kpi@gmail.com>
2020-04-29 11:13:40 +0800

    doub vs triple equal is cleared

---
+++ double-vs-trple-equal.js
@@ -0,0 +1,17 @@
+// const first = 2;
+// const second = true;
+
+// if(first == second){
+//     console.log('condtion is true');
+// }else{
+//     console.log('condition is false')        //return false
+// }
+
+const first = 0;
+const second = false;
+
+if(first == second){
+    console.log('condtion is true');
+}else{
+    console.log('condition is false')
+}
