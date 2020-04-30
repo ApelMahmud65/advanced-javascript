8fa044ff6ec3c68f708435ddb6da3b8e42d43f4e
ApelMahmud65 <apelmahmud.kpi@gmail.com>
2020-04-30 08:09:56 +0800

    about scope and something hoisting

---
+++ scope.js
@@ -0,0 +1,21 @@
+let bonus = 10;
+
+function sum(first, second){
+    let result = first + second + bonus;
+
+    // console.log(bonus);
+    if(result > 9){
+        var mood = 'happy';
+        mood = 'cranky'
+        console.log(mood);
+    }
+    console.log(mood)
+    console.log(day)
+    let day = 'friday';
+    console.log(day)
+    return result;
+}
+
+const output = sum(3, 5);
+// console.log(bonus)
+console.log(output)
